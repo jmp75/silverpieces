7180b31ca29ae1139c0b39d504aad7595c92fab2
Ben Leighton <Ben.Leighton@csiro.au>
2019-07-08 14:53:06 +1000

    untested refactored placeholders

--- silverpieces/functions.py
+++ silverpieces/functions.py
@@ -1,2 +1,18 @@
 def test():
-    print("Hello World")
+   print("Hello World")
+
+def mean_all(start_date, end_date, variable_name):
+   '''
+   get mean for entire dataset 
+   '''
+   result = ds[variable_name].sel(time=slice(pd.to_datetime(start_date), pd.to_datetime(end_date))).mean(dim=('time'))
+   return result
+
+
+
+def mean_catchment_mask_number(start_date, end_date, variable_name, catchment_mask_number):
+   '''
+   get mean for entire dataset 
+   '''
+   result = ds[variable_name].sel(time=slice(pd.to_datetime(start_date), pd.to_datetime(end_date))).where(ds.mask == catch_num).mean(dim=('time'))
+   return result
